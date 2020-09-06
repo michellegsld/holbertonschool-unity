9ad2d3eef5cc8f9497ce77cfd2f0d91921b5f6b7
Michelle Giraldo <michellegsld@gmail.com>
2020-09-06 18:31:17 -0400

    [Update] Fix movements and jumping mechanics to flow a little better

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class for controlling the Player
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]    // In order to see it in the inspector
@@ -9,9 +10,9 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 inputVector;
 
-    private bool canJump;
-    public float speed = 10f;
-    public float jump = 5f;
+    private bool onPlatform = true;
+    public float speed = 15f;
+    public float jump = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -22,21 +23,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputVector = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed); // Keys inputed
         //transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z)); <-make character look in direction it moving in
-        if(Input.GetKeyUp(KeyCode.Space))
+    }
+
+    // Called according to the framerate
+    private void FixedUpdate()
+    {
+        player.velocity = inputVector; // Move player according to keys inputed before
+        if (Input.GetKey(KeyCode.Space) && onPlatform == true)
         {
-            canJump = true;
+            player.AddForce(new Vector3(0, jump, 0), ForceMode.VelocityChange);
         }
     }
 
-    private void FixedUpdate()
+    // Called when the player collides with an object
+    void OnCollisionEnter(Collision collision)
     {
-        player.velocity = inputVector;
-        if(canJump)
+        // If the player is on a Platform
+        if (collision.gameObject.transform.parent.name == "Platforms")
         {
-            canJump = false;
-            player.AddForce(Vector3.up * Mathf.Sqrt(10f * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            onPlatform = true;
         }
+    }
+
+    // Called when the player is not touching another object anymore
+    void OnCollisionExit(Collision collision)
+    {
+        onPlatform = false;
     }
 }
