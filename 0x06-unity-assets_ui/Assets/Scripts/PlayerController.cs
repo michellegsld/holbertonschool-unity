bd9e207c57e683c11d63116bf50545659211746a
Michelle Giraldo <michellegsld@gmail.com>
2020-09-22 02:58:11 -0400

    [Update] Complete pause canvas

--- 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Class for controlling the Player
 public class PlayerController : MonoBehaviour
