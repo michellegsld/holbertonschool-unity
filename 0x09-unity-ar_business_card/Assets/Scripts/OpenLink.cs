092804050a6958fbfd15cc8a2884165cb959d30b
Michelle Giraldo <michellegsld@gmail.com>
2020-11-08 17:33:18 -0500

    [Update] Add everything have so far, test the link to see if open on click

---
+++ 0x09-unity-ar_business_card/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void SpriteClick()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/michelle-giraldo-759990193/");
+        Debug.Log("is this working?");
+    }
+}
