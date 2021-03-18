98e7729c8516e6328649f896e103cf8a5b50aaf4
Michelle Giraldo <michellegsld@gmail.com>
2021-03-18 17:44:26 -0400

    [Update] Fix git and commit issues, fix issue regarding volume sliders

---
+++ 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Class for pausing the game
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    private static Stopwatch timer;
+    public AudioMixerSnapshot play;
+    public AudioMixerSnapshot paused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If user presses ESC while pause menu is open
+        if (Input.GetKeyDown("escape"))
+        {
+            Resume();
+        }
+    }
+
+    // Pauses the game
+    public void Pause()
+    {
+        // Sets Time Scale to 0 to "freeze" any movement
+        Time.timeScale = 0;
+
+        // Muffle all sound
+        paused.TransitionTo(0.0f);
+
+        // Sets camera turn speed to 0 to prevent looking around
+        CameraController.turnSpeed = 0;
+
+        if (Timer.timer != null)
+        {
+            // Pause the timer
+            Timer.timer.Stop();
+        }
+
+        // Display pause menu
+        pauseCanvas.gameObject.SetActive(true);
+
+        // To utilize mouse pointer
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Continues the game
+    public void Resume()
+    {
+        // Activates Time Scale to normal value
+        Time.timeScale = 1;
+
+        // Un-muffle all sound
+        play.TransitionTo(0.0f);
+
+        // Sets camera turn speed to 1 to resume looking around
+        CameraController.turnSpeed = 1;
+
+        if (Timer.timer != null)
+        {
+            // Resumes the timer
+            Timer.timer.Start();
+        }
+
+        // Hides the pause menu
+        pauseCanvas.gameObject.SetActive(false);
+
+        // To set mouse pointer to game settings
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Start the current level over again
+    public void Restart()
+    {
+        // Gets current scene
+        Scene level = SceneManager.GetActiveScene();
+
+        // Un-muffle all sound
+        play.TransitionTo(0.0f);
+
+        // Re-loads the current scene by getting its name
+        SceneManager.LoadScene(level.name);
+    }
+
+    // Opens main menu
+    public void MainMenu()
+    {
+        // Un-muffle all sound
+        play.TransitionTo(0.0f);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Opens options menu
+    public void Options()
+    {
+        // Un-muffle all sound
+        play.TransitionTo(0.0f);
+
+        SceneManager.LoadScene("Options");
+    }
+}
---
+++ 0x08-unity-audio/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Class for controlling the Player
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]    // In order to see it in the inspector
+    private Rigidbody player;
+    private Transform mainCam;
+    private Animator currAnim;     // Usually called: anim
+
+    private bool onPlatform = true;
+    private bool isJumping = false;
+    private bool isFalling = false;
+    private bool canMove = true;
+    public float speed = 10f;
+    public int collisions = 0;
+
+    public GameObject pauseCanvas;
+    public GameObject winCanvas;
+
+    // For movement
+    float turnSmoothVelocity;
+    float speedSmoothVelocity;
+    float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set player, main camera, and animator
+        player = GetComponent<Rigidbody>();
+        mainCam = Camera.main.transform;
+        currAnim = player.transform.GetChild(0).gameObject.GetComponent<Animator>();
+
+        // To use the back button in the options menu
+        PlayerPrefs.SetString("lastLoadedScene", SceneManager.GetActiveScene().name);
+
+        // Activates Time Scale to normal value
+        Time.timeScale = 1;
+
+        // Sets camera turn speed to 1 to resume looking around
+        CameraController.turnSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Main purpose: to reset canMove after FallingDown sub-state, which means it's not falling anymore
+        if (Animator.StringToHash("Base Layer.Happy Idle") == currAnim.GetCurrentAnimatorStateInfo(0).fullPathHash)
+        {
+            canMove = true;
+            isFalling = false;
+        }
+
+        if (canMove)
+        {
+            // Controls player movement
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 playerMove = new Vector3(x, 0f, z).normalized;
+
+            // If input is greater than 0
+            if (playerMove != Vector3.zero)
+            {
+                float playerRotation = Mathf.Atan2(playerMove.x, playerMove.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+                player.transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(player.transform.eulerAngles.y, playerRotation, ref turnSmoothVelocity, 0.0f); // 0.05f to immediately turn w/o circling
+            }
+
+            // Last part is 0.0f to rid of icy motion, magnitude to tell if it moved
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, speed * playerMove.magnitude, ref speedSmoothVelocity, 0.0f);
+
+            player.transform.Translate(player.transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+            // Now player can still move while jumping/falling w/o triggering movement animations
+            if (!isJumping && !isFalling && onPlatform)
+            {
+                if (currentSpeed == 0)
+                {
+                    currAnim.SetTrigger("RunningToIdleTrigger");
+                }
+                else
+                {
+                    currAnim.SetTrigger("IdleToRunningTrigger");
+                }
+            }
+        }
+
+        // In order to change Jump animation into Falling animation
+        if (player.transform.position.y <= -3 && isJumping)
+        {
+            currAnim.SetTrigger("JumpToFallingTrigger");
+
+            isJumping = false;
+            isFalling = true;
+        }
+
+        // Once the player hits a certain point, they will respawn
+        if (player.transform.position.y <= -15)
+        {
+            // Instead of 0, 15, 0 -> so can't move off platform and cause loop
+            player.transform.position = new Vector3(0, 13, -2);
+        }
+
+        // Opens pause menu
+        if (Input.GetKeyDown("escape") && winCanvas.gameObject.activeSelf == false)
+        {
+            pauseCanvas.GetComponent<PauseMenu>().Pause();
+        }
+    }
+
+    // Called according to the framerate
+    private void FixedUpdate()
+    {
+        // For the user to jump, while they are on the platform and they can move
+        if (Input.GetKey(KeyCode.Space) && onPlatform && canMove)
+        {
+            isJumping = true;
+
+            Vector3 movement = new Vector3(0, 0.0F, 0) * speed * Time.deltaTime;
+            movement.y = 5.5f;
+            player.velocity = movement;
+        }
+    }
+
+    // Called when the player collides with an object
+    void OnCollisionEnter(Collision collision)
+    {
+        collisions++;
+
+        // If they are not on a platform and collide with one
+        if (onPlatform == false && collision.gameObject.tag == "Platform")
+        {
+            if (currentSpeed == 0 && isJumping)
+            {
+                currAnim.SetTrigger("JumpToIdleTrigger");
+            }
+            else if (isJumping)
+            {
+                currAnim.SetTrigger("JumpToRunningTrigger");
+            }
+            else if (canMove && isFalling)   // This means they hit the starter platform
+            {
+                canMove = false;
+                currAnim.SetTrigger("FallingToImpactTrigger");
+            }
+
+            onPlatform = true;
+            isJumping = false;
+        }
+    }
+
+    // Called when the player is not touching another object anymore
+    void OnCollisionExit(Collision collision)
+    {
+        collisions--;
+
+        // If they were on a platform and un-collided
+        if (onPlatform && collision.gameObject.tag == "Platform")
+        {
+            onPlatform = false;
+            if (currentSpeed == 0 && isJumping)
+            {
+                currAnim.SetTrigger("IdleToJumpTrigger");
+            }
+            else if (isJumping)
+            {
+                currAnim.SetTrigger("RunningToJumpTrigger");
+            }
+            else if (canMove && collisions == 0)
+            {
+                isFalling = true;
+                currAnim.SetTrigger("RunningToFallingTrigger");
+            }
+        }
+    }
+}
